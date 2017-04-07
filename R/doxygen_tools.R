70b6c39d62fd7023273f858c089c5fd3e6a30caa
nevrome <clemens@nevrome.de>
2017-04-07 23:38:28 +0200

    minor changes to documentation, renaming and an additional function to edit doxygen config files

--- R/doxygen_tools.R
+++ R/doxygen_tools.R
@@ -1,10 +1,11 @@
 #' Replace a value for a given tag on file in memory
 #' 
-#' Scan the lines and change the value for the named tag if one line has this tag,
-#' add a line at the end if no line has this tag and return a warning if several lines
-#' matching the tag
+#' Scan the lines and change the value for the named tag if one line has 
+#' this tag, add a line at the end if no line has this tag and return a 
+#' warning if several lines matching the tag.
 #' 
-#' @param fileStrings A vector with each string containing a line of the file
+#' @param fileStrings A vector with each string containing a line of the 
+#'                    file
 #' @param tag The tag to be searched for
 #' @param newVal The new value for the tag
 #' 
@@ -42,16 +43,16 @@ replace_tag <- function (fileStrings, tag, newVal) {
 #' 
 #' @param rootFolder The root path of the R package
 #' 
-#' @return NULL
+#' @return TRUE
 #' 
 #' @examples
 #' 
 #' \dontrun{
-#' dox_init()
+#' doxy_init()
 #' }
 #' 
 #' @export
-dox_init <- function (rootFolder = ".") {
+doxy_init <- function (rootFolder = ".") {
   doxyFileName <- "Doxyfile"
   initFolder <- getwd()
   if (rootFolder != ".") {
@@ -71,16 +72,46 @@ dox_init <- function (rootFolder = ".") {
   doxyfile <- replace_tag(doxyfile, "EXTRACT_ALL",      "YES")
   doxyfile <- replace_tag(doxyfile, "INPUT",            "src/")
   doxyfile <- replace_tag(doxyfile, "OUTPUT_DIRECTORY", "inst/doxygen/")
-  doxyfile <- replace_tag(doxyfile, "EXTRACT_PRIVATE",  "YES")
   cat(doxyfile, file = doxyFileName, sep = "\n")
   setwd(initFolder)
-  return(NULL)
+  return(TRUE)
 }
 
-#' devtools document function when using doxygen
+#' Edits an existing Doxyfile
 #' 
-#' Overwrites devtools::document() to include the treatment of
-#' doxygen documentation in src/
+#' Changes options in doxygen config files.  
+#' 
+#' @param pathToDoxyfile relative path to the Doxyfile
+#'                       Default: "./inst/doxygen/Doxyfile"
+#' @param options named vector with new settings. 
+#'                A list of options can be found here:
+#'                \url{https://www.stack.nl/~dimitri/doxygen/manual/config.html}    
+#'    
+#' @return TRUE
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' doxy_edit(options = c("EXTRACT_PRIVATE" = "YES"))
+#' }
+#' 
+#' @export
+doxy_edit <- function (
+  pathToDoxyfile = "./inst/doxygen/Doxyfile",
+  options = c()
+  ) {
+  doxyfile <- readLines(pathToDoxyfile)
+  for (i in 1:length(options)) {
+    doxyfile <- replace_tag(doxyfile, names(options)[i], options[i])
+  }
+  cat(doxyfile, file = pathToDoxyfile, sep = "\n")
+  return(TRUE)
+}
+
+#' Calls doxygen for an R package
+#' 
+#' Triggers doxygen documentation for the code in src/. Triggers also 
+#' the setup (with \code{doxy_init()}) before the first run. 
 #' 
 #' @param doxygen A boolean: should doxygen be ran on documents in src?
 #'                Default: TRUE if a src folder exist and FALSE if not
@@ -100,7 +131,7 @@ doxy <- function(doxygen = file.exists("src"), roxygen = FALSE) {
   if (doxygen) {
     doxyFileName <- "inst/doxygen/Doxyfile"
     if (!file.exists(doxyFileName)) {
-      dox_init()
+      doxy_init()
     }
     system(paste("doxygen", doxyFileName))
   }
