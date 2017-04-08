e80e5133c948d22aefed1030bbc0d40032483eab
nevrome <clemens@nevrome.de>
2017-04-08 12:05:15 +0200

    better documentation + comments and another option for doxy()

--- R/doxygen_tools.R
+++ R/doxygen_tools.R
@@ -1,49 +1,56 @@
-#' Replace a value for a given tag on file in memory
+#' Updates and adds doxygen options in a line string vector   
 #' 
-#' Scan the lines and change the value for the named tag if one line has 
-#' this tag, add a line at the end if no line has this tag and return a 
-#' warning if several lines matching the tag.
+#' Scans the lines and changes the value for the named tag if one line has 
+#' this tag, adds a line at the end if no line has this tag and returns a 
+#' warning if several lines match the tag.
 #' 
 #' @param fileStrings A vector with each string containing a line of the 
 #'                    file
-#' @param tag The tag to be searched for
-#' @param newVal The new value for the tag
+#' @param tag  A string with the tag to be searched for
+#' @param newVal A string with the new value for the tag
 #' 
 #' @return The vector of strings with the new value
 #' 
 replace_tag <- function (fileStrings, tag, newVal) {
+  
+  # get and count lines with the tag
   iLine  <- grep(paste0("^", tag, "\\>"), fileStrings)
   nLines <- length(iLine)
+  
   if (nLines == 0){
+    # if tag is not present, add it with its value at the bottom
     line <- paste0(tag, "\t= ", newVal)
     iLine <- length(fileStrings) + 1
   } else if (nLines > 0){
+    # if tag is present once, replace its value
     line <- gsub("=.*", paste0("= ", newVal), fileStrings[iLine])
     if(nLines > 1){
+      # if tag is present multiple times, do nothing and throw warning
       warning(paste0(
         "File has", nLines, 
-        "for key", tag, 
-        "check it up manually"
+        "for key", tag, ". ",
+        "Check it up manually."
       ))
     }
   }
   fileStrings[iLine] <- line
+  
   return(fileStrings)
 }
 
 #' Prepares the R package structure for use with doxygen
 #' 
-#' Makes a configuration file in inst/doxygen and set a few options:
+#' Makes a configuration file in inst/doxygen/ and sets a few options:
 #'     \itemize{
 #'        \item{EXTRACT_ALL = YES}
 #'        \item{INPUT = src/}
 #'        \item{OUTPUT_DIRECTORY = inst/doxygen/}
-#'        \item{EXTRACT_PRIVATE = YES}
 #'     }
 #' 
-#' @param rootFolder The root path of the R package
+#' @param rootFolder A string with the path to the root directory of the R
+#'                   package. Default: "."
 #' 
-#' @return TRUE
+#' @return NULL
 #' 
 #' @examples
 #' 
@@ -53,12 +60,18 @@ replace_tag <- function (fileStrings, tag, newVal) {
 #' 
 #' @export
 doxy_init <- function (rootFolder = ".") {
+  
   doxyFileName <- "Doxyfile"
+  
+  # move to root directory
   initFolder <- getwd()
   if (rootFolder != ".") {
     setwd(rootFolder)
   }
+  
+  # check if DESCRIPTION file is present
   rootFileYes <- length(grep("DESCRIPTION", dir())) > 0
+  
   # prepare the doxygen folder
   doxDir <- "inst/doxygen"
   if (!file.exists(doxDir)) {
@@ -66,7 +79,7 @@ doxy_init <- function (rootFolder = ".") {
   }
   setwd(doxDir)
   
-  # prepare the doxygen configuration file
+  # prepare the doxygen configuration file with the initial settings
   system(paste0("doxygen -g ", doxyFileName))
   doxyfile <- readLines("Doxyfile")
   doxyfile <- replace_tag(doxyfile, "EXTRACT_ALL",      "YES")
@@ -74,20 +87,22 @@ doxy_init <- function (rootFolder = ".") {
   doxyfile <- replace_tag(doxyfile, "OUTPUT_DIRECTORY", "inst/doxygen/")
   cat(doxyfile, file = doxyFileName, sep = "\n")
   setwd(initFolder)
-  return(TRUE)
+  
+  return(NULL)
 }
 
 #' Edits an existing Doxyfile
 #' 
 #' Changes options in doxygen config files.  
 #' 
-#' @param pathToDoxyfile relative path to the Doxyfile
+#' @param pathToDoxyfile A string with the relative path to the Doxyfile.
 #'                       Default: "./inst/doxygen/Doxyfile"
-#' @param options named vector with new settings. 
+#' @param options A named vector with new settings. The names represent
+#'                the tags.
 #'                A list of options can be found here:
 #'                \url{https://www.stack.nl/~dimitri/doxygen/manual/config.html}    
 #'    
-#' @return TRUE
+#' @return NULL
 #' 
 #' @examples
 #' 
@@ -100,28 +115,34 @@ doxy_edit <- function (
   pathToDoxyfile = "./inst/doxygen/Doxyfile",
   options = c()
   ) {
+  
   doxyfile <- readLines(pathToDoxyfile)
+  
+  # loop to apply replace_tag() for every element of the vector
   if (length(options) != 0) {
     for (i in 1:length(options)) {
       doxyfile <- replace_tag(doxyfile, names(options)[i], options[i])
     }
   }
   cat(doxyfile, file = pathToDoxyfile, sep = "\n")
-  return(TRUE)
+  
+  return(NULL)
 }
 
 #' Calls doxygen for an R package
 #' 
 #' Triggers doxygen documentation for the code in src/. Triggers also 
-#' the setup (with \code{doxy_init()}) before the first run. 
+#' the setup (with \code{doxy_init()}) at the first run. 
 #' 
-#' @param doxygen A boolean: should doxygen be ran on documents in src?
+#' @param doxygen A boolean: should doxygen be ran on documents in src/?
 #'                Default: TRUE if a src folder exist and FALSE if not
 #' @param roxygen A boolean: should devtools::document() be ran after the 
 #'                creation of the doxygen documentation?
 #'                Default: FALSE
+#' @param pathToDoxyfile A string with the relative path to the Doxyfile.
+#'                       Default: "./inst/doxygen/Doxyfile"
 #' 
-#' @return The value returned by devtools::document()
+#' @return NULL or the value returned by devtools::document()
 #' 
 #' @examples
 #' \dontrun{
@@ -129,15 +150,24 @@ doxy_edit <- function (
 #' }
 #' 
 #' @export
-doxy <- function(doxygen = file.exists("src"), roxygen = FALSE) {
+doxy <- function(
+  doxygen = file.exists("src"), 
+  roxygen = FALSE,
+  pathToDoxyfile = "./inst/doxygen/Doxyfile"
+  ) {
+  
+  # doxygen
   if (doxygen) {
-    doxyFileName <- "inst/doxygen/Doxyfile"
+    doxyFileName <- pathToDoxyfile
     if (!file.exists(doxyFileName)) {
       doxy_init()
     }
     system(paste("doxygen", doxyFileName))
   }
+  
+  # roxygen
   if (roxygen) {
     devtools::document()
   }
+  
 }
