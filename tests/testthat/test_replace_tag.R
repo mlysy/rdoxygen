600c7a51e5caef5ee96a91cb17f447ce483a323f
nevrome <clemens@nevrome.de>
2017-04-08 12:05:53 +0200

    test structuring

--- tests/testthat/test_replace_tag.R
+++ tests/testthat/test_replace_tag.R
@@ -2,14 +2,22 @@ context("Tests of tag replacement function")
 
 fakeFileStrings <- c("Hello = world", "SURE\t= indeed", "Hello = you")
 
-expect_warning(replace_tag(fakeFileStrings, "Hello", "me"))
+test_that("replace_tag throws warning, if a tag appears multiple times", {
+  expect_warning(replace_tag(fakeFileStrings, "Hello", "me"))
+})
 
 newFake <- replace_tag(fakeFileStrings, "SURE", "me")
-expect_equal(length(newFake), length(fakeFileStrings))
-expect_equal(length(grep("SURE", newFake)), 1)
-expect_equal(length(grep("me", newFake)), 1)
+
+test_that("replace_tag actually replaces values", {
+  expect_equal(length(newFake), length(fakeFileStrings))
+  expect_equal(length(grep("SURE", newFake)), 1)
+  expect_equal(length(grep("me", newFake)), 1)
+})
 
 newFake <- replace_tag(fakeFileStrings, "Bouh", "frightened?")
-expect_equal(length(newFake), length(fakeFileStrings) + 1)
-expect_equal(length(grep("Bouh", newFake)), 1)
-expect_equal(length(grep("frightened?", newFake)), 1)
+
+test_that("replace_tag adds previously unknown tags at the bottom", {
+  expect_equal(length(newFake), length(fakeFileStrings) + 1)
+  expect_equal(length(grep("Bouh", newFake)), 1)
+  expect_equal(length(grep("frightened?", newFake)), 1)
+})
