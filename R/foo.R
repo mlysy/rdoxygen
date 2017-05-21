c012a3bf7a6aaca097252aa9d3e8df6b0d9625b6
nevrome <clemens@nevrome.de>
2017-05-21 22:30:29 +0200

    reduced c example code to a bare minimum

--- R/foo.R
+++ R/foo.R
@@ -1,21 +1,9 @@
-#' foo
+#' c_test
 #' 
 #' @export
-foo <- function(x, type = c("C", "Fortran", "Call")) {
-    type <- match.arg(type)
+c_test <- function(x) {
     stopifnot(is.numeric(x))
-
-    if (type == "Fortran") {
-        out <- .Fortran(C_foo, x = as.double(x), n = length(x))
-        return(out$x)
-    }
-    if (type == "C") {
-        out <- .C(C_bar, x = as.double(x), n = length(x))
-        return(out$x)
-    }
-    if (type == "Call") {
-        out <- .Call(C_baz, x = as.double(x))
-        return(out)
-    }
+    out <- .C(C_bar, x = as.double(x), n = length(x))
+    return(out$x)
 }
 
