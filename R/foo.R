6fd3f2f17a010d27c0147620b5934825eba9e796
nevrome <clemens@nevrome.de>
2017-05-21 22:21:43 +0200

    added a minimal source code example (https://github.com/cjgeyer/foo)

---
+++ R/foo.R
@@ -0,0 +1,21 @@
+#' foo
+#' 
+#' @export
+foo <- function(x, type = c("C", "Fortran", "Call")) {
+    type <- match.arg(type)
+    stopifnot(is.numeric(x))
+
+    if (type == "Fortran") {
+        out <- .Fortran(C_foo, x = as.double(x), n = length(x))
+        return(out$x)
+    }
+    if (type == "C") {
+        out <- .C(C_bar, x = as.double(x), n = length(x))
+        return(out$x)
+    }
+    if (type == "Call") {
+        out <- .Call(C_baz, x = as.double(x))
+        return(out)
+    }
+}
+
---
+++ src/init.c
@@ -0,0 +1,40 @@
+
+// See Sections 5.4 and 6.15 of Writing R Extensions
+//
+// Remove styles, which are deprecated in R-3.3.3
+
+#include <Rdefines.h>
+#include <R_ext/Rdynload.h>
+#include <R_ext/Visibility.h>
+#include "foo.h"
+
+static R_NativePrimitiveArgType bar_types[2] = {REALSXP, INTSXP};
+
+static R_NativePrimitiveArgType qux_types[4] =
+    {INTSXP, REALSXP, REALSXP, REALSXP};
+
+static R_CMethodDef cMethods[] = {
+    {"bar", (DL_FUNC) &bar, 2, bar_types},
+    {"qux", (DL_FUNC) &qux, 4, qux_types},
+    {NULL, NULL, 0, NULL}
+};
+ 
+static R_CallMethodDef callMethods[]  = {
+    {"baz", (DL_FUNC) &baz, 1},
+    {"quux", (DL_FUNC) &quux, 3},
+    {NULL, NULL, 0}
+};
+
+static R_FortranMethodDef fortranMethods[] = {
+    {"foo", (DL_FUNC) &F77_SUB(foo), 2, bar_types},
+    {"quf", (DL_FUNC) &F77_SUB(quf), 4, qux_types},
+    {NULL, NULL, 0, NULL}
+};
+ 
+void attribute_visible R_init_rdoxygen(DllInfo *info)
+{
+    R_registerRoutines(info, cMethods, callMethods, fortranMethods, NULL);
+    R_useDynamicSymbols(info, FALSE);
+    R_forceSymbols(info, TRUE);
+}
+
